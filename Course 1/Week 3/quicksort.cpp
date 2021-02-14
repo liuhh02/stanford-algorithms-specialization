f0af1e1ca3dc688ce14d31eb68d1da49684498d1
Liu Haohui <liuhh02@gmail.com>
2021-02-14 23:48:38 +0800

    Update quicksort.cpp

--- Course 1/Week 3/quicksort.cpp
+++ Course 1/Week 3/quicksort.cpp
@@ -2,31 +2,60 @@
 
 using namespace std;
 
-int partition(int arr[], int l, int r) {
+void swap(int arr[], int a, int b) {
+    int temp = arr[b];
+    arr[b] = arr[a];
+    arr[a] = temp;
+}
+
+int findMedian(int arr[], int l, int r) {
+    int n = r-l+1;
+    int pos = 0;
+    if (n % 2 == 0) {
+        pos = n/2 - 1 + l;
+    } else {
+        pos = n/2 + l; 
+    }
+    int a = arr[l];
+    int b = arr[pos];
+    int c = arr[r];
+    if (a > b && a < c) {
+        return l;
+    } else if (b > a && b < c) {
+        return pos;
+    } else {
+        return r;
+    }
+}
+
+int partition(int arr[], int l, int r, int pi) {
+    if (pi != 0) swap(arr, l, pi);
     int pivot = arr[l];
     int i = l+1;
     for (int j = l+1; j <= r; j++) {
         if (arr[j] < pivot) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            swap(arr, i, j);
             i++;
         }
     }
-    arr[l] = arr[i-1];
-    arr[i-1] = pivot;
+    swap(arr, l, i-1);
     return i-1;
 }
 
 void quickSort(int arr[], int l, int r) {
     if (r <= l) return;
-    int position = partition(arr, l, r);
+    // first element of the array is the pivot element:
+    // int position = partition(arr, l, r, 0);
+    // last element of the array is the pivot element:
+    // int position = partition(arr, l, r, r); 
+    // median element is the pivot element:
+    int position = partition(arr, l, r, findMedian(arr, l, r)); 
     quickSort(arr, 0, position-1);
     quickSort(arr, position+1, r);
 }
 
 int main() {
-    int arr[] = { 1, 20, 6, 4, 5, 9, 100 };
+    int arr[] = { 3, 2, 10, 6, 7, 1, 9, 5, 4, 8 };
     int arr_size = sizeof(arr) / sizeof(arr[0]);
 
     quickSort(arr, 0, arr_size-1);
